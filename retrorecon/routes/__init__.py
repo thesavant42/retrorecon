215e36de8683ac49e65c4dedba2db26fdbd9f091
thesavant42 <jbrashars@gmail.com>
2025-06-15 22:06:19 -0700

    Add REST API client module

--- retrorecon/routes/__init__.py
+++ retrorecon/routes/__init__.py
@@ -3,5 +3,6 @@ from .tools import bp as tools_bp
 from .db import bp as db_bp
 from .settings import bp as settings_bp
 from .urls import bp as urls_bp
+from .api_client import bp as api_client_bp
 
-__all__ = ['notes_bp', 'tools_bp', 'db_bp', 'settings_bp', 'urls_bp']
+__all__ = ['notes_bp', 'tools_bp', 'db_bp', 'settings_bp', 'urls_bp', 'api_client_bp']
